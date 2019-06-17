958f891c0230ca2eb573db6937b7de1fa78ade40
pilotvitalik <itvkip@yandex.ru>
2019-06-17 21:35:15 +0300

    Add Event when change checkbox

---
+++ Event_when_change_checkbox/Index.js
@@ -0,0 +1,6 @@
+  function showOrHide(cb, cat) {
+    cb = document.getElementById(cb);
+    cat = document.getElementById(cat);
+    if (cb.checked) cat.style.display = "block";
+    else cat.style.display = "none";
+  }
